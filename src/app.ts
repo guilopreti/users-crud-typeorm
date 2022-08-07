62bded876ee6c43e0142b1a92af75869fe2226eb
Guilherme <guilherme.llopreti@gmail.com>
2022-08-07 00:59:35 -0300

    feat: deploy configs

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App running!");
 });
